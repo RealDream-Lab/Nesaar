d3285f4f5b97dee5c1e51107311bfaaa2d083503
Mehdi Hassani | مهدی حسنی <hassani.mehdi@gmail.com>
2025-11-23 07:40:19 +0330

    اصلاح متن برچسب نوع کاربر از "عوامل اجرائی" به "عضو اجرائی"

--- index.php
+++ index.php
@@ -79,7 +79,7 @@ if ($licenseStatus['valid'] !== true) {
                     </div>
                     <div class="form-check form-check-inline">
                       <label class="form-check-label" for="coworkerType">
-                        عوامل اجرائی
+                        عضو اجرائی
                       </label>
                       <input class="form-check-input" type="radio" name="userType" id="coworkerType" value="coworker">
                     </div>
