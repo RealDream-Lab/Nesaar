d4c9eb121f7a93bac672b1305553458e5475f840
Mehdi Hassani | مهدی حسنی <hassani.mehdi@gmail.com>
2025-12-31 10:38:48 +0330

    رفع مشکل عدم نمایش منوی ملزومات در کاربر گزارش گیر

--- Recipient/index.php
+++ Recipient/index.php
@@ -54,6 +54,7 @@ $bodyClass = $wavesAnimationDisabled ? 'class="no-waves-animation"' : '';
     <link rel="stylesheet" href="../assets/bootstrap/bootstrap.min.css">
     <link rel="stylesheet" href="../assets/fonts/vazir/vazir.css">
     <link rel="stylesheet" href="../assets/sweetalert2/sweetalert2.min.css">
+    <link rel="stylesheet" href="../assets/sweetalert2/swal-animations.css">
     <link rel="stylesheet" href="../assets/app/style.css">
 </head>
 
@@ -197,6 +198,7 @@ $bodyClass = $wavesAnimationDisabled ? 'class="no-waves-animation"' : '';
     <script src="../assets/sweetalert2/sweetalert2.min.js"></script>
     <script src="../assets/app/version.js"></script>
     <script src="../assets/vendor/chartjs/chart.min.js"></script>
+    <script src="../assets/app/swal-helper.js"></script>
     <script src="dashboard.js"></script>
     <script src="../dashboard/dashboard.js"></script>
 </body>
